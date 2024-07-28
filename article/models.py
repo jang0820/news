465b686f918b14d18516359ac7aecdfb087bb23e
jang0820 <jang0820@gmail.com>
2024-07-28 20:07:40 +0800

    修改文字刪除多出的函式庫

--- article/models.py
+++ article/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 class Article(models.Model):
     title = models.CharField(max_length=100, verbose_name='title', help_text='news title')
